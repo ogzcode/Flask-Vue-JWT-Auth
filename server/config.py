57b0891f9b692051b399f72db3679ed9157bb81f
ogzCode <oguzguc44@gmail.com>
2023-09-04 10:18:59 +0300

    Application created

---
+++ server/config.py
@@ -0,0 +1,11 @@
+import os
+from datetime import timedelta
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = "secret-key"
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
